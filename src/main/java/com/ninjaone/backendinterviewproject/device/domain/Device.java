158d89a97c0c2598e5c62ad14659590bbb10cf9b
breno39 <idd_balmeida@uolinc.com>
2022-07-21 22:00:15 -0300

    implemented base device model

--- src/main/java/com/ninjaone/backendinterviewproject/device/domain/Device.java
+++ src/main/java/com/ninjaone/backendinterviewproject/device/domain/Device.java
@@ -1,2 +1,11 @@
-package com.ninjaone.backendinterviewproject.device.domain;public class Device {
+package com.ninjaone.backendinterviewproject.device.domain;
+
+import com.ninjaone.backendinterviewproject.commons.domain.AbstractEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Device extends AbstractEntity {
+    private String systemName;
+    private DeviceType type;
 }
--- src/main/java/com/ninjaone/backendinterviewproject/device/domain/DeviceType.java
+++ src/main/java/com/ninjaone/backendinterviewproject/device/domain/DeviceType.java
@@ -1,2 +1,5 @@
-package com.ninjaone.backendinterviewproject.device.domain;public enum DeviceType {
+package com.ninjaone.backendinterviewproject.device.domain;
+
+public enum DeviceType {
+    WINDOWS_WORKSTATION, WINDOWS_SERVER, MAC, UBUNTU
 }
