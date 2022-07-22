d4c55c811b2f92395dff584f3270125153ce18df
breno39 <idd_balmeida@uolinc.com>
2022-07-21 21:59:49 -0300

    implemented base service model

--- src/main/java/com/ninjaone/backendinterviewproject/service/domain/Service.java
+++ src/main/java/com/ninjaone/backendinterviewproject/service/domain/Service.java
@@ -1,2 +1,16 @@
-package com.ninjaone.backendinterviewproject.service.domain;public class Service {
+package com.ninjaone.backendinterviewproject.service.domain;
+
+import com.ninjaone.backendinterviewproject.commons.domain.AbstractEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Service extends AbstractEntity {
+    private Integer cost;
+    private ServiceType type;
+
+    public Service(Integer cost, ServiceType type) {
+        this.cost = cost;
+        this.type = type;
+    }
 }
--- src/main/java/com/ninjaone/backendinterviewproject/service/domain/ServiceType.java
+++ src/main/java/com/ninjaone/backendinterviewproject/service/domain/ServiceType.java
@@ -1,2 +1,11 @@
-package com.ninjaone.backendinterviewproject.service.domain;public enum ServiceType {
+package com.ninjaone.backendinterviewproject.service.domain;
+
+public enum ServiceType {
+    WINDOWS_ANTIVIRUS(5), MAC_ANTIVIRUS(7), SCREENSHARE(1), BACKUP(3), PSA(2);
+
+    private final Integer cost;
+
+    ServiceType(Integer cost) {
+        this.cost = cost;
+    }
 }
